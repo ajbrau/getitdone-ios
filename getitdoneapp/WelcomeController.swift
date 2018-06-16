808903da4ae92ca8e40199a0383fa0353a4a50d5
AJ Call <AJCall710@gmail.com>
2018-06-16 14:39:55 -0600

    added custom label class

--- getitdoneapp/WelcomeController.swift
+++ getitdoneapp/WelcomeController.swift
@@ -15,16 +15,27 @@ class WelcomeController: UIViewController {
         view.layer.cornerRadius = 6
         return view
     }()
-
+    
+    let titleLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
+        titleLabel.text = "GET IT DONE"
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(bg)
         bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         bg.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
         bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        
+        bg.addSubview(titleLabel)
+        titleLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: bg.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: bg.topAnchor, constant: 60).isActive = true
     }
 
 }
