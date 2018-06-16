63940abca61e6a3aa8a2e99c3541d65b50a05f19
AJ Call <AJCall710@gmail.com>
2018-06-16 14:20:10 -0600

    added hex code extension convenience

--- getitdoneapp/GDGradient.swift
+++ getitdoneapp/GDGradient.swift
@@ -11,8 +11,13 @@ import UIKit
 class GDGradient: UIView {
     
     var colors:[CGColor] = [
-        UIColor.init(red: 58, green: 123, blue: 213).cgColor,
-        UIColor.init(red: 100, green: 228, blue: 255).cgColor
+        UIColor.init(red: 100, green: 228, blue: 255).cgColor,
+        UIColor.init(red: 58, green: 123, blue: 213).cgColor
+    ]
+    
+    var hexColors:[CGColor] = [
+        UIColor.init(rgb: 0x64E4FF).cgColor,
+        UIColor.init(rgb: 0x3A7BD5).cgColor
     ]
     
     override init(frame: CGRect) {
@@ -20,7 +25,7 @@ class GDGradient: UIView {
         
         self.translatesAutoresizingMaskIntoConstraints = false
         if let layer = self.layer as? CAGradientLayer {
-            layer.colors = self.colors
+            layer.colors = self.hexColors
             layer.locations = [0.0, 1.2]
         }
         
